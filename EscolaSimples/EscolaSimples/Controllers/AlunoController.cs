e14827fd1b1df0eb9d6ddcbc5b83820abe2e64d3
Tayná Carvalho <taynacarvalhogomes@gmail.com>
2026-08-16 16:09:11 -0400

    add

---
+++ EscolaSimples/EscolaSimples/Controllers/AlunoController.cs
@@ -0,0 +1,21 @@
+﻿using EscolaSimples.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscolaSimples.Controllers
+{
+    public class AlunoController : Controller
+    {
+        
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Criar(Aluno aluno)
+        {
+            return Content($"Aluno '{aluno.Nome}' com matrícula '{aluno.Matricula}' recebido com sucesso!");
+        }
+    }
+}
